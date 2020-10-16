1d829d837b3b61dfa7f6399bae15df2356c2602c
Alê Neri <alessandro.neri@gmail.com>
2020-10-16 20:06:05 -0300

    Scafold Front-End

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small='Nós'></PageHeader>
+        <h2>Nossa história</h2>
+        <p>Lorem ipsum dolor sit amet....</p>
+        <h2>Visão e Missão</h2>
+        <p>Lorem ipsum dolor sit amet....</p>
+        <h2>Contatos</h2>
+        <p>Lorem ipsum dolor sit amet....</p>
+    </div>
+)
